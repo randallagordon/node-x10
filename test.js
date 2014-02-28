370bd11f7e27804f1634ef0bafd4298be8f62e2e
Randall A. Gordon <randall@randallagordon.com>
2014-02-28 15:27:32 -0800

    Tweak to get test.js working on personal system

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var options = require("./settings");
 var x10 = require("./lib/cm11");
 
@@ -5,39 +7,30 @@ var cm11 = new x10.CM11(options);
 cm11.on("open", function() {
 	console.log("CM11 is open. get status");
 	cm11.getStatus();
-	
-        unitOn();
-        unitOff();
-        unitOn();
-        unitOff();
-        unitOn();
-        unitOff();
-        unitOn();
-        unitOff();
         
-	// setTimeout(function() {
-	    // unitOn();
-	// }, 1000);
+  setTimeout(function() {
+    unitOn();
+  }, 1000);
 });
 
-var house = x10.HOUSE.A;
-var unit = x10.UNIT.UNIT_5;
+var house = x10.HOUSE.O;
+var unit = x10.UNIT.UNIT_10;
 
 function unitOn() {
-    console.log("sending on");
-    cm11.sendCommand({house: house, unit: unit}, x10.FUNCTION.ON, function(err) {
-        console.log("returned from sending command: on");
-        // setTimeout(function() {
-            // unitOff();
-        // }, 1000);
-    });
+  console.log("sending on");
+  cm11.sendCommand({house: house, unit: unit}, x10.FUNCTION.ON, function(err) {
+    console.log("returned from sending command: on");
+    setTimeout(function() {
+      unitOff();
+    }, 1000);
+  });
 }
 function unitOff() {
-    console.log("sending off");
-    cm11.sendCommand({house: house, unit: unit}, x10.FUNCTION.OFF, function(err) {
-        console.log("returned from sending command: off");
-        // setTimeout(function() {
-            // unitOn();
-        // }, 1000);
-    });
+  console.log("sending off");
+  cm11.sendCommand({house: house, unit: unit}, x10.FUNCTION.OFF, function(err) {
+    console.log("returned from sending command: off");
+    setTimeout(function() {
+      unitOn();
+    }, 1000);
+  });
 }
